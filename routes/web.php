62c1a8bf3d25e441ed0f69bf9be67aea4c5be752
Paige Jones <hello@paigejones.me>
2021-08-10 22:00:01 +0100

    resort routes

--- routes/web.php
+++ routes/web.php
@@ -16,25 +16,37 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', ['as' => 'home', 'uses' => 'HomeController']);
 
-Route::get('/register', ['as' => 'register', 'uses' => 'AuthController@create']);
-Route::post('/register', ['as' => 'register', 'uses' => 'AuthController@store']);
-Route::get('/login', ['as' => 'login', 'uses' => 'AuthController@edit']);
-Route::post('/login', ['as' => 'login', 'uses' => 'AuthController@update']);
-Route::get('/logout', ['as' => 'logout', 'uses' => 'AuthController@destroy']);
+// auth routes
+Route::group(['middleware' => 'guest'], function() {
+    Route::get('/register', ['as' => 'register', 'uses' => 'AuthController@create']);
+    Route::post('/register', ['as' => 'register', 'uses' => 'AuthController@store']);
+    Route::get('/login', ['as' => 'login', 'uses' => 'AuthController@edit']);
+    Route::post('/login', ['as' => 'login', 'uses' => 'AuthController@update']);
+});
+Route::get('/logout', ['as' => 'logout', 'middleware' => ['auth'], 'uses' => 'AuthController@destroy']);
 
+// snippet routes
 Route::group(['as' => 'snippets.', 'prefix' => 'snippets'], function() {
-    Route::get('/', ['as' => 'index', 'middleware' => ['auth'], 'uses' => 'SnippetController@index']);
-    Route::get('/create', ['as' => 'create', 'middleware' => ['auth'], 'uses' => 'SnippetController@create']);
-    Route::post('/create', ['as' => 'store', 'middleware' => ['auth'], 'uses' => 'SnippetController@store']);
-    Route::get('/{snippet_id}', ['as' => 'show', 'uses' => 'SnippetController@show']);
-    Route::get('/{snippet_id}/approve', ['as' => 'approve', 'middleware' => ['auth'], 'uses' => 'SnippetController@approve']);
-    Route::get('/{snippet_id}/unapprove', ['as' => 'unapprove', 'middleware' => ['auth'], 'uses' => 'SnippetController@unapprove']);
-    Route::get('/{snippet_id}/edit', ['as' => 'edit', 'middleware' => ['auth'], 'uses' => 'SnippetController@edit']);
-    Route::patch('/{snippet_id}/edit', ['as' => 'update', 'middleware' => ['auth'], 'uses' => 'SnippetController@update']);
-    Route::delete('{snippet_id}/delete', ['as' => 'destroy', 'middleware' => ['auth'], 'uses' => 'SnippetController@destroy']);
+    Route::group(['middleware' => ['auth']], function() {
+        Route::get('/', ['as' => 'index', 'uses' => 'SnippetController@index']);
+        Route::get('/create', ['as' => 'create', 'uses' => 'SnippetController@create']);
+        Route::post('/create', ['as' => 'store', 'uses' => 'SnippetController@store']);
+    });
+
+    Route::group(['prefix' => '{snippet_id}'], function() {
+        Route::get('/', ['as' => 'show', 'uses' => 'SnippetController@show']);
+        Route::group(['middleware' => ['auth']], function() {
+            Route::get('/approve', ['as' => 'approve', 'uses' => 'SnippetController@approve']);
+            Route::get('/unapprove', ['as' => 'unapprove', 'uses' => 'SnippetController@unapprove']);
+            Route::get('/edit', ['as' => 'edit', 'uses' => 'SnippetController@edit']);
+            Route::patch('/edit', ['as' => 'update', 'uses' => 'SnippetController@update']);
+            Route::delete('/delete', ['as' => 'destroy', 'uses' => 'SnippetController@destroy']);
+        });
+    });
 });
 
-Route::group(['as' => 'invites.', 'prefix' => 'invites'], function() {
+// invite routes
+Route::group(['as' => 'invites.', 'middleware' => ['auth'], 'prefix' => 'invites'], function() {
     Route::get('/', ['as' => 'index', 'uses' => 'InviteController@index']);
     Route::get('/create', ['as' => 'create', 'uses' => 'InviteController@store']);
 });
